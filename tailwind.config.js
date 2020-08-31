c441df99e5ef28c1f9276be887771a4ae1f38e08
xrishi20 <xrishi20@gmail.com>
2020-09-01 01:21:43 +0530

    oof

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  purge: [],
+  theme: {
+    fontFamily: {
+      primary:
+        'HelveticaNeue, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
+      heading:
+        'Averta, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
+    },
+    extend: {
+      colors: {
+        dGreen: "#1D3641",
+        grGray: "#C8DDE7",
+        grBlue: "#90BFD0",
+        grlGray: "#F5F6F8",
+        grdGray: "#ECEEF0",
+        pGray: "#ECF6F9",
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+  future: {
+    removeDeprecatedGapUtilities: true,
+  },
+};
