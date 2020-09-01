50072fde03df87a080f3a42908230e153a9ac560
xrishi20 <xrishi20@gmail.com>
2020-09-01 16:12:58 +0530

    wady hek tepes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,28 @@
 module.exports = {
   purge: [],
   theme: {
+    colors: {
+      transparent: "transparent",
+      black: "#000000",
+      white: "#ffffff",
+      dGreen: "#1D3641",
+      grlBlue: "#C8DDE7",
+      grdBlue: "#90BFD0",
+      grlGray: "#F5F6F8",
+      grdGray: "#ECEEF0",
+      pGray: "#ECF6F9",
+      dGray: "#E5E5E5",
+      sRed: "#E5A29A",
+      pRed: "#EF8274",
+      pYellow: "#EDBB5E",
+    },
     fontFamily: {
       primary:
         'HelveticaNeue, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
       heading:
         'Averta, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
     },
-    extend: {
-      colors: {
-        dGreen: "#1D3641",
-        grGray: "#C8DDE7",
-        grBlue: "#90BFD0",
-        grlGray: "#F5F6F8",
-        grdGray: "#ECEEF0",
-        pGray: "#ECF6F9",
-      },
-    },
+    extend: {},
   },
   variants: {},
   plugins: [],
